6b937f6556f7b6962273c9ff350a424dbfd703be
ZhnecRF <92275257+ZhnecRF@users.noreply.github.com>
2025-11-07 17:12:14 +0300

    Update App.vue

--- src/App.vue
+++ src/App.vue
@@ -1,31 +1,13 @@
 <template>
-  <div>
-    <nav class="navbar navbar-expand-lg navbar-dark bg-dark">
-      <div class="container-fluid">
-        <router-link class="navbar-brand" to="/">Price Monitor</router-link>
-
-        <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navMain">
-          <span class="navbar-toggler-icon"></span>
-        </button>
-
-        <div class="collapse navbar-collapse" id="navMain">
-          <ul class="navbar-nav me-auto mb-2 mb-lg-0">
-            <li class="nav-item">
-              <router-link class="nav-link" to="/">Дашборд</router-link>
-            </li>
-            <li class="nav-item">
-              <router-link class="nav-link" to="/items">Товары</router-link>
-            </li>
-          </ul>
-        </div>
+  <div class="container py-4">
+    <nav class="navbar navbar-dark bg-dark rounded px-3 mb-4">
+      <span class="navbar-brand mb-0 h4">Price Monitor</span>
+      <div class="d-flex gap-3">
+        <router-link class="text-white text-decoration-none" to="/">Дашборд</router-link>
+        <router-link class="text-white text-decoration-none" to="/items">Товары</router-link>
       </div>
     </nav>
 
-    <main class="container py-3">
-      <router-view />
-    </main>
+    <router-view />
   </div>
 </template>
-
-<script setup lang="ts">
-</script>
