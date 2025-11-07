28024001c38bf0435061edefdb2ffd9222fbc8c6
ZhnecRF <92275257+ZhnecRF@users.noreply.github.com>
2025-11-07 17:13:34 +0300

    Update main.ts

--- src/main.ts
+++ src/main.ts
@@ -1,12 +1,13 @@
 import { createApp } from 'vue'
+import './style.css'
 import App from './App.vue'
 import { router } from './router'
 
 import 'bootstrap/dist/css/bootstrap.min.css'
 import 'bootstrap'
-import './style.css'
 
-const app = createApp(App)
+createApp(App).use(router).mount('#app')
+
 
 // подключаем роутер — это делает <router-link> кликабельными
 app.use(router)
