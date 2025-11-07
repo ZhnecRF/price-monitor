0a4ef786a30a543a82353fd9e548d02ff982af86
ZhnecRF <92275257+ZhnecRF@users.noreply.github.com>
2025-11-07 16:52:03 +0300

    Update index.ts

--- electron/server/index.ts
+++ electron/server/index.ts
@@ -11,15 +11,12 @@ export let io: Server
 
 export async function startServer() {
   const app = express()
-  app.use('/api/items', itemsRoute)
   app.use(cors())
   app.use(express.json({ limit: '2mb' }))
 
-  // health-check
   app.get('/api/health', (_req, res) => res.json({ ok: true, ts: Date.now() }))
-
-  // ▼ добавь:
   app.use('/api/groups', groupsRoute)
+  app.use('/api/items', itemsRoute)
 
   const server = http.createServer(app)
   io = new Server(server, { cors: { origin: '*' } })
