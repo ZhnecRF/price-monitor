12c78f8b07ef9aedd936aecd5d000e0c109fb029
ZhnecRF <92275257+ZhnecRF@users.noreply.github.com>
2025-11-07 14:55:21 +0300

    Update App.vue

--- src/App.vue
+++ src/App.vue
@@ -2,14 +2,20 @@
   <div>
     <nav class="navbar navbar-expand-lg navbar-dark bg-dark">
       <div class="container-fluid">
-        <a class="navbar-brand" href="#/">Price Monitor</a>
+        <router-link class="navbar-brand" to="/">Price Monitor</router-link>
+
         <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navMain">
           <span class="navbar-toggler-icon"></span>
         </button>
+
         <div class="collapse navbar-collapse" id="navMain">
           <ul class="navbar-nav me-auto mb-2 mb-lg-0">
-            <li class="nav-item"><a class="nav-link" href="#/">Дашборд</a></li>
-            <li class="nav-item"><a class="nav-link" href="#/items">Товары</a></li>
+            <li class="nav-item">
+              <router-link class="nav-link" to="/">Дашборд</router-link>
+            </li>
+            <li class="nav-item">
+              <router-link class="nav-link" to="/items">Товары</router-link>
+            </li>
           </ul>
         </div>
       </div>
