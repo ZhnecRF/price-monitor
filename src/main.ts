d4344c800658a184ba696ee861c36e77c1cc43f2
ZhnecRF <92275257+ZhnecRF@users.noreply.github.com>
2025-11-07 14:48:29 +0300

    Update main.ts

--- src/main.ts
+++ src/main.ts
@@ -2,6 +2,9 @@ import { createApp } from 'vue'
 import './style.css'
 import App from './App.vue'
 
+import 'bootstrap/dist/css/bootstrap.min.css'
+import 'bootstrap'
+
 createApp(App).mount('#app').$nextTick(() => {
   // Use contextBridge
   window.ipcRenderer.on('main-process-message', (_event, message) => {
